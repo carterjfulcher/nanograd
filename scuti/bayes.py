a82b4cf837f7b4667d428888b560718b9d625ba9
Carter Fulcher <carter@fulchereng.com>
2021-04-14 22:07:42 -0600

    framed bayes algorithm

--- scuti/bayes.py
+++ scuti/bayes.py
@@ -4,8 +4,22 @@ cfulcher
 A recursive algorithm to filter out noise 
 '''
 
+import numpy as np 
+
 class Bayes(Object): 
-    def __init__(self): 
+    def __init__(self, model: np.array, measurements: np.array) -> None:
+        self._model = model
+        self._measurements = measurements 
+    
+    @classmethod 
+    def forward(self): #predict the next position
+        pass 
+    
+    @classmethod
+    def feed(self, position: np.array, measurement: np.array) -> None: 
+        ''' Feeds next step and calls self.forward() '''
+        pass 
+        
         
 
 
