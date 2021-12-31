86595966d9ff8c8695f4a7bf1ab7f4a93bae0015
carterjfulcher <carter@fulchereng.com>
2021-12-30 17:48:19 -0700

    forward pass testing for bayes classifier

--- scuti/classifiers/test/test_bayes_classifier.py
+++ scuti/classifiers/test/test_bayes_classifier.py
@@ -1,12 +1,9 @@
 import numpy as np 
 
-from scuti.utils.distribution import Distribution
-
-
 def test_classifier():
     import pandas as pd 
     import numpy as np
-    from scuti.bayes import BayesClassifier
+    from scuti.classifiers import BayesClassifier
 
     df = pd.read_csv('sample_data/500_Person_Gender_Height_Weight_Index.csv')
     train_y = df['Gender'].apply(lambda x: 0 if x == 'Male' else 1).values
@@ -14,10 +11,15 @@ def test_classifier():
 
     model = BayesClassifier()
     model.fit(train_x, train_y)
-
-
+    
     assert(isinstance(model._variances, np.ndarray))
     assert(isinstance(model._means, np.ndarray))
+
+    test_x = np.array([187, 89, 2])
+
+    test_y = model.forward(test_x)
+
+    assert(test_y in [0, 1])
 
 if __name__ == "__main__":
     test_classifier()
